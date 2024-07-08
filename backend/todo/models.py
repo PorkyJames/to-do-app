177b7d441f874acb7951ec985687da83bee8c5c4
PorkyJames <kwanjko17@gmail.com>
2024-07-08 15:16:08 -0700

    Added Model and Set SQLite3 as my db

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=25, required=True)
+    description = models.CharField(max_length=255, required=False)
+    completed = models.BooleanField()
+    created_at = models.DateField()
