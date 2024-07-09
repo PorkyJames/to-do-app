4dacc8e7ed4c49db61b61d0bb48977f8a22d0e59
PorkyJames <kwanjko17@gmail.com>
2024-07-09 15:24:12 -0700

    Finshed draft structure for AllTasksList

--- frontend/src/components/AllTasksList/AllTasksList.jsx
+++ frontend/src/components/AllTasksList/AllTasksList.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 
-const AllTasksList = () => {
+//! This component will list all the tasks that our user creates or has already been created in our database
+
+const AllTasksList = ( {tasks} ) => {
+
     return (
         <>
-            <h1> All Tasks List!</h1>
+            <h3>List of Tasks to do</h3>
+            //! If the tasks length is greater than 0, then we'll create an unorderedlist and map through listing each task and
+            //! its index then throw them into a list where each key is the index and the task itself is displayed.
+            {tasks.length > 0 ? (
+                <ul>
+                    {tasks.map((task,index) => {
+                        <li key={index}>{task}</li>
+                    })}
+                </ul>
+            ) : (
+                //! Else, we list a p tag stating that no tasks have been listed yet. 
+                <p> No tasks listed yet!</p>
+            )}
         </>
     )
 }
---
+++ frontend/src/components/DeleteTaskModal/DeleteTaskModal.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+const DeleteTaskModal = () => {
+    return (
+        <h1>Delete Task Modal</h1>
+    )
+}
+
+export default DeleteTaskModal;
