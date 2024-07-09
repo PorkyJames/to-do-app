8269a6a20f5cbef54bf3bcb4243cd3d9078af125
PorkyJames <kwanjko17@gmail.com>
2024-07-08 17:51:09 -0700

    Got seeders to finally work and seed into my sqlite db

--- README.md
+++ README.md
@@ -32,3 +32,13 @@ formats such as JSON. This is basically like the app.py in Flask where we're ret
 - def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 - to call the view, we need to map it to a URL
+
+
+Nice, so then let's move onto setting up the DB / Creating the model
+- First we're going to make sure that the db is set up correctly. Since this is a small time project, we want to be able to 
+- use SQLite3 instead of Postgres. Fortunately, in our settings.py, SQLite3 is already set up for us when we started our django app
+- if we were to use Postgres instead, we'd need to make some changes to our DATABASE variable, but we'll leave as it is for now.
+- THen we need to create our Model for our Task. Once we've created our Task Model, we need to register it with our admin.py.
+- Lastly! We need to add it to our INSTALLED_APPS in our settings.py.
+
+Once that's completed, run python manage.py makemigrations / migrate and check out sqlite3 db to see if our task populated.
---
+++ my_project/todo/seed.py
@@ -0,0 +1,27 @@
+import os
+import django
+import sys
+
+from datetime import datetime
+
+# Add the project root directory to the Python path
+# Had to add an extra pathway to find my todo folder / seed properly
+sys.path.append('/home/kwanj/projects/To-Do/my_project') 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
+from todo.models import Task
+
+# List of tasks to add to our card
+tasks = [
+    {"title": "Grocery Shopping", "description": "Buy milk, eggs, and bread", "completed": False, "created_at": datetime(2024,7,8)},
+    {"title": "Read Book", "description": "Read 'Django for APIs' by William S. Vincent", "completed": False, "created_at": datetime(2024,7,5)},
+    {"title": "Jogging", "description": "Jog around the park for 30 minutes.", "completed": True, "created_at": datetime(2024,7,7)},
+]
+
+for task in tasks:
+    Task.objects.create(**task)
+    #** is the unpack feature which will unpack our task variable listed above for us.
+
+print("Data Seeded Successfully")
