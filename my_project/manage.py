ee0bf6570782c811320af7b8ed9e4b6e09e64eac
PorkyJames <kwanjko17@gmail.com>
2024-07-11 14:52:27 -0700

    Trying to fix seed problems on EC2

--- my_project/manage.py
+++ my_project/manage.py
@@ -6,6 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
+
+    sys.path.append('/home/kwanj/projects/To-Do/my_project') 
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
