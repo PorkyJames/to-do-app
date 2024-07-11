d45f674ee5ac5c1a0f4d921d3087fc5055c98c49
PorkyJames <kwanjko17@gmail.com>
2024-07-11 14:50:38 -0700

    Added sys path append line to wsgi.py in the backend to fix backend module not found issue on EC2

--- my_project/backend/wsgi.py
+++ my_project/backend/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/kwanj/projects/To-Do/my_project') 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
