6872771f6eaa9b250c23526a3373f3e62108dd1c
PorkyJames <kwanjko17@gmail.com>
2024-07-12 10:16:54 -0700

    Added additional exceptions in ALLOWED hosts

--- my_project/backend/settings.py
+++ my_project/backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     # '35.86.247.213',
-
+    '52.37.198.17',
     'localhost',
     'http://jamestodoappbucket.s3-website-us-west-2.amazonaws.com'
 
@@ -66,6 +66,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
     # '35.86.247.213',
+    'http://52.37.198.17:8000'
     'http://jamestodoappbucket.s3-website-us-west-2.amazonaws.com'
 ]
 
